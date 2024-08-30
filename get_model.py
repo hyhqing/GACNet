66f42e800679d2acbe51ec63be904a5e64f1d954
hyhqing <hyhqing@163.com>
2024-08-30 16:08:17 +0800

    Update get_model.py

--- get_model.py
+++ get_model.py
@@ -10,52 +10,52 @@ def get_model(args, models='danet'):
 
     assert models in ['transunet', 'hrcnet', 'segformer_b0', 'segformer_b1', 'segformer_b2', 'segformer_b3',
                       'segformer_b4', 'segformer_b5', 'cswinT', 'cswinS', 'cswinB', 'cswinL', 'efficientnetv2',
-                      'deeplabv3plus', 'fcn', 'unet', 'MFIANet_es', 'MFIANet_em', 'MFIANet_el', 'MFIANet_b0',
-                      'MFIANet_b1', 'MFIANet_b2', 'MFIANet_b3', 'MFIANet_b4', 'MFIANet_b5']
+                      'deeplabv3plus', 'fcn', 'unet', 'GACNet_es', 'GACNet_em', 'GACNet_el', 'GACNet_b0',
+                      'GACNet_b1', 'GACNet_b2', 'GACNet_b3', 'GACNet_b4', 'GACNet_b5']
 
-    if models == 'MFIANet_b0':
+    if models == 'GACNet_b0':
         from model.backbone.MFIANet_mit import MFIANet_b0
-        model = MFIANet_b0(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
+        model = GACNet_b0(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
                            head_type=args.head_type).cuda()
 
-    if models == 'MFIANet_b1':
-        from model.backbone.MFIANet_mit import MFIANet_b1
-        model = MFIANet_b1(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
+    if models == 'GACNet_b1':
+        from model.backbone.GACNet_mit import GACNet_b1
+        model = GACNet_b1(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
                            head_type=args.head_type).cuda()
 
-    if models == 'MFIANet_b2':
-        from model.backbone.MFIANet_mit import MFIANet_b2
+    if models == 'GACNet_b2':
+        from model.backbone.GACNet_mit import GACNet_b2
         model = MFIANet_b2(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
                            head_type=args.head_type).cuda()
 
-    if models == 'MFIANet_b3':
-        from model.backbone.MFIANet_mit import MFIANet_b3
-        model = MFIANet_b3(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
+    if models == 'GACNet_b3':
+        from model.backbone.GACNet_mit import GACNet_b3
+        model = GACNet_b3(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
                            head_type=args.head_type).cuda()
 
-    if models == 'MFIANet_b4':
-        from model.backbone.MFIANet_mit import MFIANet_b4
-        model = MFIANet_b4(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
+    if models == 'GACNet_b4':
+        from model.backbone.GACNet_mit import GACNet_b4
+        model = GACNet_b4(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
                            head_type=args.head_type).cuda()
 
-    if models == 'MFIANet_b5':
-        from model.backbone.MFIANet_mit import MFIANet_b5
-        model = MFIANet_b5(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
+    if models == 'GACNet_b5':
+        from model.backbone.GACNet_mit import GACNet_b5
+        model = GACNet_b5(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
                            head_type=args.head_type).cuda()
 
-    if models == 'MFIANet_es':
-        from model.backbone.MFIANet_efficientnet import MFIANet_s
-        model = MFIANet_s(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
+    if models == 'GACNet_es':
+        from model.backbone.GACNet_efficientnet import GACNet_s
+        model = GACNet_s(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
                           head_type=args.head_type).cuda()
 
-    if models == 'MFIANet_em':
-        from model.backbone.MFIANet_efficientnet import MFIANet_m
-        model = MFIANet_m(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
+    if models == 'GACNet_em':
+        from model.backbone.GACNet_efficientnet import GACNet_m
+        model = GACNet_m(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
                           head_type=args.head_type).cuda()
 
-    if models == 'MFIANet_el':
-        from model.backbone.MFIANet_efficientnet import MFIANet_l
-        model = MFIANet_l(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
+    if models == 'GACNet_el':
+        from model.backbone.GACNet_efficientnet import GACNet_l
+        model = GACNet_l(num_classes=args.num_classes, in_chans=args.data_channel, aux=args.use_aux, head=args.head,
                           head_type=args.head_type).cuda()
 
     if models == 'deeplabv3plus':
